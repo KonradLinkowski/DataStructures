56999901dc123c08ac5fdd8d34d5076176a807ec
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-05-10 19:54:16 +0200

    Adds List interface.

---
+++ src/algoritms/List.java
@@ -0,0 +1,15 @@
+package algoritms;
+
+public interface List <T> {
+	int getSize();
+	
+	T get(int index) throws IndexOutOfBoundsException;
+	
+	void set(T object, int index) throws IndexOutOfBoundsException;
+	
+	void add(T object);
+	
+	void insert(T object, int index) throws IndexOutOfBoundsException;
+	
+	T remove(int index) throws IndexOutOfBoundsException;
+}
