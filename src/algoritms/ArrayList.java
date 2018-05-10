8b792c7abd2042aa92be0f8e82fbfbcfd05e8f62
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-05-10 21:39:49 +0200

    Adds ArrayList implementation of List.

---
+++ src/algoritms/ArrayList.java
@@ -0,0 +1,84 @@
+package algoritms;
+
+public class ArrayList<T> implements List<T> {
+
+	private int count = 0;
+	private T[] elements;
+	
+	@SuppressWarnings("unchecked")
+	public ArrayList() {
+		elements = (T[]) new Object[8];
+	}
+	
+	@Override
+	public int getSize() {
+		return count;
+	}
+
+	@Override
+	public T get(int index) throws IndexOutOfBoundsException {
+		if (index >= count) {
+			throw new IndexOutOfBoundsException();
+		}
+		return elements[index];
+	}
+
+	@Override
+	public void set(T object, int index) throws IndexOutOfBoundsException {
+		if (index >= count) {
+			throw new IndexOutOfBoundsException();
+		}
+		elements[index] = object;
+	}
+
+	@Override
+	public void add(T object) {
+		tryToResize();
+		elements[count] = object;
+		count++;
+	}
+
+	@Override
+	public void insert(T object, int index) throws IndexOutOfBoundsException {
+		if (index > count) {
+			throw new IndexOutOfBoundsException();
+		}
+		tryToResize();
+		for (int i = count; i > index; i--) {
+			elements[i] = elements[i - 1];
+		}
+		count++;
+		elements[index] = object;
+	}
+
+	@Override
+	public T remove(int index) throws IndexOutOfBoundsException {
+		if (index >= count) {
+			throw new IndexOutOfBoundsException();
+		}
+		T temp = elements[index];
+		tryToResize();
+		for (int i = index; i < count; i++) {
+			elements[i] = elements[i + 1];
+		}
+		count--;
+		return temp;
+	}
+
+	@SuppressWarnings("unchecked")
+	private void tryToResize() {
+		if (count == elements.length) {
+			T[] array = (T[]) new Object[elements.length * 2];
+			for (int i = 0; i < count; i++) {
+				array[i] = elements[i];
+			}
+			elements = array;
+		} else if (count < elements.length / 4) {
+			T[] array = (T[]) new Object[elements.length / 4];
+			for (int i = 0; i < count; i++) {
+				array[i] = elements[i];
+			}
+			elements = array;
+		}
+	}
+}
