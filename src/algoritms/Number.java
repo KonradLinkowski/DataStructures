2c6bf079ab8d6e1214facb41e8465b697e155855
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-05-12 13:08:00 +0200

    Adds float constructor to Number class.

--- src/algoritms/Number.java
+++ src/algoritms/Number.java
@@ -4,9 +4,16 @@ public class Number {
 	private long numerator;
 	private long denominator;
 	
-	public Number(long numerator) {
-		this.numerator = numerator;
+	public Number(long number) {
+		this.numerator = number;
 		this.denominator = 1;
+	}
+	
+	public Number(double number) {
+		Number temp = floatToFraction(number);
+		temp.normalize();
+		this.numerator = temp.numerator;
+		this.denominator = temp.denominator;
 	}
 	
 	public Number(long numerator, long denominator) {
@@ -45,6 +52,15 @@ public class Number {
 		this.denominator /= gcd;
 	}
 	
+	public static Number floatToFraction(double number) {
+		long denominator = 1;
+		while (number != (long)number) {
+			number *= 10;
+			denominator *= 10;
+		}
+		return new Number((long)number, denominator);
+	}
+	
 	public static long lcm(long a, long b) {
 		return a / gcd(a, b) * b;
 	}
@@ -57,6 +73,32 @@ public class Number {
 			b = c;
 		}
 		return a;
+	}
+	
+	public long getNumerator() {
+		return this.numerator;
+	}
+	
+	public Number setNumerator(long number) {
+		this.numerator = number;
+		return this;
+	}
+	
+	public long getDenominator() {
+		return this.denominator;
+	}
+	
+	public Number setDenominator(long number) {
+		this.denominator = number;
+		return this;
+	}
+	
+	public Double toFloat() {
+		return (double) (this.numerator / this.denominator);
+	}
+	
+	public Double toDouble() {
+		return (double) (this.numerator / this.denominator);
 	}
 	
 	@Override
